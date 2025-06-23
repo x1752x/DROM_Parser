7d98d0217a0d0282d6099834de30f2fec063a456
Vladislav <vlad_kadun@vk.com>
2025-06-23 11:30:12 +0700

    add multithread proccessing

--- DromParser.py
+++ DromParser.py
@@ -1,4 +1,8 @@
 import json
+import threading
+from asyncio import as_completed
+from concurrent.futures.thread import ThreadPoolExecutor
+
 import requests
 
 from bs4 import BeautifulSoup
@@ -20,13 +24,15 @@ class DromParser:
     def __scrape_links(self, pages: list[str]) -> list[str]:
         links = []
 
-        for page in pages:
-            response = requests.get(page)
-            html = response.text
-            soup = BeautifulSoup(html, 'lxml')
-            titles = soup.find_all(attrs={"data-ftid": "bull_title"})
-            for title in titles:
-                links.append(title.attrs.get("href"))
+        with ThreadPoolExecutor() as executor:
+            futures = [executor.submit(requests.get, page) for page in pages]
+            for future in futures:
+                response = future.result()
+                html = response.text
+                soup = BeautifulSoup(html, 'lxml')
+                titles = soup.find_all(attrs={"data-ftid": "bull_title"})
+                for title in titles:
+                    links.append(title.attrs.get("href"))
 
         return links
 
@@ -39,22 +45,24 @@ class DromParser:
         data = json.loads(script)
         result["primary_date"] = data["report"]["fields"][2]["data"][0].split(' ')[1]
         result["title"] = data["pageMeta"]["titleBull"]
+        result["link"] = link
         return result
 
     def __validate_links(self, links: list[str]) -> dict[str, str]:
         result = {}
-        for link in links:
-            try:
-                meta = self.__retrieve_primary_date(link)
-                pdate = int(meta["primary_date"])
-            except (TypeError, ValueError, IndexError) as _:
-                continue
+        with ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self.__retrieve_primary_date, link) for link in links]
+            for future in futures:
+                try:
+                    meta = future.result()
+                    pdate = int(meta["primary_date"])
+                except (TypeError, ValueError, IndexError) as _:
+                    continue
 
-            if pdate < self.settings.primary_from or pdate > self.settings.primary_to:
-                continue
+                if pdate < self.settings.primary_from or pdate > self.settings.primary_to:
+                    continue
 
-            result[meta["title"]] = link
-
+                result[meta["title"]] = meta["link"]
         return result
 
     def parse(self) -> dict[str, str]:
